b22bd8686e17460f317e72eddf160244ebf56f56
HaraldurShoshan <haraldurshoshan@gmail.com>
2016-11-08 11:46:19 +0000

    Added Procfile

--- build/scripts/ITraveled.bat
+++ build/scripts/ITraveled.bat
@@ -63,7 +63,7 @@ set CMD_LINE_ARGS=%*
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\ITraveled.jar;%APP_HOME%\lib\slf4j-api-1.7.21.jar
+set CLASSPATH=%APP_HOME%\lib\ITraveled.jar;%APP_HOME%\lib\slf4j-api-1.7.21.jar;%APP_HOME%\lib\spark-core-2.5.jar;%APP_HOME%\lib\jetty-server-9.3.6.v20151106.jar;%APP_HOME%\lib\jetty-webapp-9.3.6.v20151106.jar;%APP_HOME%\lib\websocket-server-9.3.6.v20151106.jar;%APP_HOME%\lib\websocket-servlet-9.3.6.v20151106.jar;%APP_HOME%\lib\javax.servlet-api-3.1.0.jar;%APP_HOME%\lib\jetty-http-9.3.6.v20151106.jar;%APP_HOME%\lib\jetty-io-9.3.6.v20151106.jar;%APP_HOME%\lib\jetty-xml-9.3.6.v20151106.jar;%APP_HOME%\lib\jetty-servlet-9.3.6.v20151106.jar;%APP_HOME%\lib\websocket-common-9.3.6.v20151106.jar;%APP_HOME%\lib\websocket-client-9.3.6.v20151106.jar;%APP_HOME%\lib\websocket-api-9.3.6.v20151106.jar;%APP_HOME%\lib\jetty-util-9.3.6.v20151106.jar;%APP_HOME%\lib\jetty-security-9.3.6.v20151106.jar
 
 @rem Execute ITraveled
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %I_TRAVELED_OPTS%  -classpath "%CLASSPATH%" com.itraveled.ITraveled %CMD_LINE_ARGS%
--- src/main/java/com/itraveled/ITraveled.java
+++ src/main/java/com/itraveled/ITraveled.java
@@ -1,11 +1,42 @@
 package com.itraveled;
 
 import com.itraveled.entities.*;
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
 
-public class ITraveled
+public class ITraveled implements SparkApplication
 {
 	public static void main(String[] args)
 	{	
+		staticFileLocation("/public");
+		SparkApplication iTraveled = new ITraveled();
 
-	}	
+		String port = System.getenv("PORT");
+        if (port != null) {
+            port(Integer.valueOf(port));
+        }
+
+        iTraveled.init();
+	}
+
+	@Override
+    public void init()
+    {
+    	post("/loginUser", (req, res) ->
+    	{
+    		User user = new User();
+
+    		final String userName = req.queryParams("userName");
+    		final String userPass = req.queryParams("userPass");
+
+    		user.setId(1);
+    		user.setName(userName);
+    		user.setPassword(userPass);
+
+    		res.status(200);
+
+    		return res;
+    	});
+    }	
 }
